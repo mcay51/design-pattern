458d8cff2746198802ec92785cac23e5952cb7c4
mcay51 <mcay51@gmail.com>
2024-03-01 12:27:55 +0300

    creational
    add creational and  add singleton

---
+++ creational/singleton/src/main/java/tr/com/mustafacay/singleton/service/Singleton.java
@@ -0,0 +1,6 @@
+package tr.com.mustafacay.singleton.service;
+
+public interface Singleton extends Cloneable{
+    public Object clone();
+
+}
---
+++ creational/src/main/java/tr/com/mustafacay/Main.java
@@ -0,0 +1,7 @@
+package tr.com.mustafacay;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
