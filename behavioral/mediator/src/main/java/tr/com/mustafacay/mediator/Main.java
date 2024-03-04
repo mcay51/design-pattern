a9a3baf914a17e1afcc550ad20775540fff70f3a
mcay51 <mcay51@gmail.com>
2024-03-04 22:35:09 +0300

    Update Main.java

--- behavioral/mediator/src/main/java/tr/com/mustafacay/mediator/Main.java
+++ behavioral/mediator/src/main/java/tr/com/mustafacay/mediator/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! Mediator Design Pattern");
-       // mediatorOrnekUI();
+        mediatorOrnekUI();
         mediatorMesajServiceOrnek();
 
     }
